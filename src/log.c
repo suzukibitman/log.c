6e452dcd5bf3c360b0ea0d23cc59d308f86b746d
bitnya <suzuki20111007@gmail.com>
2019-03-02 16:20:41 +0900

    add mili second in log

--- src/log.c
+++ src/log.c
@@ -33,11 +33,11 @@
 #define MAX_FUNC_LENGTH 64
 
 static struct {
-  void *udata;
-  log_LockFn lock;
-  FILE *fp;
-  int level;
-  int quiet;
+	void *udata;
+	log_LockFn lock;
+	FILE *fp;
+	int level;
+	int quiet;
 } L;
 
 
@@ -52,55 +52,55 @@ static struct func_info{
 static struct func_info func_list[MAX_FUNC_SAVE] = {0}; 
 
 static const char *level_names[] = {
-  "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
+	"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
 };
 
 #define LOG_USE_COLOR
 
 #ifdef LOG_USE_COLOR
 static const char *level_colors[] = {
-  "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
+	"\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
 };
 #endif
 
 static int trace_function(const int line, const char *function_name);
 
 static void lock(void)   {
-  if (L.lock) {
-    L.lock(L.udata, 1);
-  }
+	if (L.lock) {
+		L.lock(L.udata, 1);
+	}
 }
 
 
 static void unlock(void) {
-  if (L.lock) {
-    L.lock(L.udata, 0);
-  }
+	if (L.lock) {
+		L.lock(L.udata, 0);
+	}
 }
 
 
 void log_set_udata(void *udata) {
-  L.udata = udata;
+	L.udata = udata;
 }
 
 
 void log_set_lock(log_LockFn fn) {
-  L.lock = fn;
+	L.lock = fn;
 }
 
 
 void log_set_fp(FILE *fp) {
-  L.fp = fp;
+	L.fp = fp;
 }
 
 
 void log_set_level(int level) {
-  L.level = level;
+	L.level = level;
 }
 
 
 void log_set_quiet(int enable) {
-  L.quiet = enable ? 1 : 0;
+	L.quiet = enable ? 1 : 0;
 }
 
 /* TODO:
@@ -112,64 +112,75 @@ void log_set_quiet(int enable) {
 void log_log(int level, const char *function, int line, const char *fmt, ...) 
 {
 
-  if (level < L.level) 
-  {
-    return;
-  }
+	if (level < L.level) 
+	{
+		return;
+	}
 
-  /* Acquire lock */
-  //セマフォ管理しよう
-  lock();
-  int index = trace_function(line, function);
-  //printf("idnex :%d \n" ,index);
+	/* Acquire lock */
+	//セマフォ管理しよう
+	lock();
+	int index = trace_function(line, function);
+	//printf("idnex :%d \n" ,index);
 
-  /* Get current time */
-  time_t t = time(NULL);
-  struct tm *lt = localtime(&t);
+	/* Get current time */
+	time_t t = time(NULL);
+	struct tm *lt = localtime(&t);
 
-  /* Log to stderr */
-  if (!L.quiet) {
-    va_list args;
-    char buf[16];
-    buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
+	struct timeval logerTime;
+	gettimeofday(&logerTime, NULL);
+
+	/* Log to stderr */
+	if (!L.quiet) {
+		va_list args;
+		char buf[16];
+		//何この書き方・・・？
+		buf[strftime(buf, sizeof(buf), "%H:%M:%S", lt)] = '\0';
 #ifdef LOG_USE_COLOR
-    fprintf(
-      stderr, "[%s] %s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
-      LOG_WORD, buf, level_colors[level], level_names[level], function, line);
+		fprintf(
+				stderr, "[%s] %s:%06d %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
+				LOG_WORD,
+				buf,
+				logerTime.tv_usec,
+				level_colors[level],
+				level_names[level],
+				function,
+				line);
 #else
-    fprintf(stderr, 
-			"[%s] %s %-5s %s:%d:[seq]:%ld ", 
-			LOG_WORD,
-			buf, 
-			level_names[level],
-			function,
-			line,
-			func_list[index].seqCounter);
+		fprintf(stderr, 
+				"[%s] %s:%06d %-5s %s:%d:[seq]:%ld ", 
+				LOG_WORD,
+				buf, 
+				logerTime.tv_usec,
+				level_names[level],
+				function,
+				line,
+				func_list[index].seqCounter);
 #endif
-    va_start(args, fmt);//これの使い方はまたイマイチ、何これ〜〜〜
-    vfprintf(stderr, fmt, args);
-    va_end(args);
-    fprintf(stderr, "\n");
-    fflush(stderr);
-  }
+		va_start(args, fmt);//これの使い方はまたイマイチ、何これ〜〜〜
+		vfprintf(stderr, fmt, args);
+		va_end(args);
+		fprintf(stderr, "\n");
+		fflush(stderr);
+	}
 
-  /* Log to file */
-  if (L.fp) 
-  {
-    va_list args;
-    char buf[32];
-    buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", lt)] = '\0';
-    fprintf(L.fp, "%s %-5s %s:%d: ", buf, level_names[level], function, line);
-    va_start(args, fmt);
-    vfprintf(L.fp, fmt, args);
-    va_end(args);
-    fprintf(L.fp, "\n");
-    fflush(L.fp);
-  }
+	/* Log to file */
+	if (L.fp) 
+	{
+		va_list args;
+		char buf[32];
+		buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", lt)] = '\0';
+		fprintf(L.fp, "%s %-5s %s:%d: ", buf, level_names[level], function, line);
+		va_start(args, fmt);
+		vfprintf(L.fp, fmt, args);
+		va_end(args);
+		fprintf(L.fp, "\n");
+		fflush(L.fp);
+	}
 
-  /* Release lock */
-  unlock();
-  //セマフォ管理しよう THE END
+	/* Release lock */
+	unlock();
+	//セマフォ管理しよう THE END
 }
 
 
@@ -192,7 +203,7 @@ static int trace_function(const int line, const char *function_name)
 			foundFuncInList = true;
 			break;
 		}
-			i++;
+		i++;
 	}
 
 	/* バグが存在している */
